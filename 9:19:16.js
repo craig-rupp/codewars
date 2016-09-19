56b59b67acf7b88285df0bea04e928fcf63f807d
Craig Rupp <craigrupp@utexas.edu>
2016-09-19 10:01:35 -0500

    first codewars solution added

---
+++ 9:19:16.js
@@ -0,0 +1,19 @@
+use strict;
+
+
+//Jaden Case Strings - Capitalize every first word 7 kyu
+
+String.prototype.toJadenCase = function () {
+  //...
+  var arr = this.split(' ');
+  arr.forEach(function(element,index,arr){
+    var letter = element.substring(0,1);
+    element = element.replace(letter,'');
+    arr[index] = letter.toUpperCase() + element;
+  });
+var str = arr.join(' ');
+ return str;
+};
+
+
+
